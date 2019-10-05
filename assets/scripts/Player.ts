6c8aee71aefc4257bbf22a64ed5705d1d476571d
Dirck Hecking <dhecking@gmail.com>
2019-10-05 12:36:46 -0400

    code for jumping and movement

--- assets/scripts/Player.ts
+++ assets/scripts/Player.ts
@@ -2,6 +2,7 @@ const {ccclass, property} = cc._decorator;
 
 @ccclass
 export default class Player extends cc.Component {
+    jumpAction: cc.ActionInterval;
 
     @property
     jumpHeight: number = 0;
@@ -14,9 +15,24 @@ export default class Player extends cc.Component {
 
     @property
     accel: number = 0;
+
+    constructor(){
+        super();
+    }
     
+    onLoad(){
+        this.jumpAction = this.setJumpAction();
+        this.node.runAction(this.jumpAction);
+    }
+
     start () {
 
+    }
+
+    private setJumpAction(): cc.ActionInterval {
+        let ascent = cc.moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+        let decent = cc.moveBy(this.jumpDuration, cc.v2(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+        return cc.repeatForever(cc.sequence(ascent, decent));
     }
 
 }
