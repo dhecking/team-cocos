413ec80323109a47b2a43f66943c8cb0af648d59
Dirck Hecking <dhecking@gmail.com>
2019-10-06 07:50:35 -0400

    deploy

--- deploy-new.sh
+++ deploy-new.sh
@@ -1,38 +1,17 @@
 #!/usr/bin/env sh
 
-BRANCH=`git branch | grep \* | cut -d ' ' -f2`
-BUILD="/mnt/c/users/dheck/software/CocosCreator/CocosCreator.exe --path . --build 'title=${BRANCH};platform=web-mobile;buildPath=./build;debug=false;webOrientation=landscape;packageName=com.teamcocos.${BRANCH}'"
+REPO_PATH=`git rev-parse --show-toplevel`;
+REPO=`basename ${REPO_PATH}`;
+BRANCH=`git branch | grep \* | cut -d ' ' -f2`;
+BUILD="/mnt/c/users/dheck/software/CocosCreator/CocosCreator.exe --path . --build 'title=${BRANCH};platform=web-mobile;buildPath=./build;debug=false;webOrientation=landscape;packageName=com.${REPO}.${BRANCH}'";
 
-eval ${BUILD}
+eval ${BUILD};
 
-#exit 1
+cd build/web-module;
 
-git add .
-
-git commit -m'deploy'
-
-git push
-
-# abort on errors
-set -e
-
-# build
-npm run build
-
-# navigate into the build output directory
-cd build/web-module
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:dhecking/team-cocos.git star-catcher:gh-pages
+git init;
+git add -A;
+git commit -m 'deploy';
+git push -f git@github.com:dhecking/${REPO}.git ${BRANCH}:gh-pages;
 
 cd -
