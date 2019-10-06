194742123e32fdd3816e0c3ea73e215da65c03db
Dirck Hecking <dhecking@gmail.com>
2019-10-05 22:49:32 -0400

    Decorating is fun

--- assets/scripts/Game.ts
+++ assets/scripts/Game.ts
@@ -3,6 +3,21 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class Game extends cc.Component {
 
+    @property({displayName: "Score", tooltip: "Current Score"})
+    score: number = 0;
+
+    @property({displayName: "Min Existence", tooltip: "Stars must exist for given duration"})
+    minStarDuration: number = 0;
+    @property({displayName: "Max Existence", tooltip: "Stars must expire before given duration "})
+    maxStarDuration: number = 0;
+
+    @property(cc.Node)
+    ground: cc.Node = null;
+    @property(cc.Node)
+    player: cc.Node = null;
+    @property(cc.Prefab)
+    star: cc.Prefab = null;
+
     start () {
 
     }
